d966fe6f0bef54e55a2cddd1ec7f8aec1fc26b65
Laura Barros <laurajbarros@gmail.com>
2020-05-18 09:30:16 -0300

    heroku deploy

--- server/server.js
+++ server/server.js
@@ -26,10 +26,9 @@ const server = new ApolloServer({
 const app = express();
 server.applyMiddleware({ app });
 
-app.listen({ port: 4000 }, () =>
+app.listen({ port: process.env.PORT || 4000 }, () =>
   console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
 );
-
 
 module.exports = {
   dataSources,
